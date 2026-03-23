6ff0aca7ef58296b20b9e4036f9000e6d173f691
Horacio Karzon <horaciokar@gmail.com>
2026-03-23 00:08:13 -0500

    photos

--- frontend/app.js
+++ frontend/app.js
@@ -3,9 +3,24 @@ async function load() {
     const data = await res.json();
 
     const g = document.getElementById("galeria");
+    g.innerHTML = "";
 
     data.forEach(f => {
-        g.innerHTML += `<img src="${encodeURI(f.url)}" width="300">`;
+        const url = encodeURI(f.url);
+
+        const div = document.createElement("div");
+
+        const img = document.createElement("img");
+        img.src = url;
+        img.width = 300;
+
+        const p = document.createElement("p");
+        p.textContent = f.nombre;
+
+        div.appendChild(img);
+        div.appendChild(p);
+
+        g.appendChild(div);
     });
 }
 
